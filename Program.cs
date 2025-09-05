ef818a35d63f999d5cafecd3ee0ae4a25f9b954e
Gio89fran <gio89fran@gmail.com>
2025-09-05 17:21:11 +0200

    Versione definitiva per il download, conversione e lancio del job di importazione del brand hendi

--- Program.cs
+++ Program.cs
@@ -1,25 +1,39 @@
-﻿using System.Globalization;
-using System.IO;
+﻿using ClosedXML.Excel;
 using CsvHelper;
-using ClosedXML.Excel;
 using CsvHelper.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-
         var appConfig = new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                     .Build();
 
-        string csvPath = appConfig["CsvPath"];
-        string excelPath = appConfig["ExcelPath"];
-        string sheetName = appConfig["SheetName"];
+        string csvPath = appConfig["CsvPath"] ?? "";
+        string excelPath = appConfig["ExcelPath"] ?? "";
+        string sheetName = appConfig["SheetName"] ?? "";
+        string onlineFileName = appConfig["OnlineFileName"] ?? "";
+        string iQuoteConsole = appConfig["IQuoteConsole"] ?? "";
 
+        //prima scarico il file csv
+        var downloadedFile = new List<string>();
+
+        Program.DownloadAsync(onlineFileName, csvPath, downloadedFile, null);
+
+        //se non ho scaricato nulla, esco
+        if (downloadedFile.Count == 0)
+        {
+            Console.WriteLine("Nessun file scaricato. Terminazione del programma.");
+            return;
+        }
 
         using var reader = new StreamReader(csvPath);
         var config = new CsvConfiguration(CultureInfo.InvariantCulture)
@@ -52,5 +66,47 @@ class Program
 
         workbook.SaveAs(excelPath);
         Console.WriteLine($"File Excel salvato in: {excelPath}");
+
+        //una volta salvato il file, posso eliminare il file csv
+        try
+        {
+            csv.Dispose();
+            File.Delete(csvPath);
+        }
+        catch (Exception)
+        {
+
+        }
+        //quando finito, lancio la procedura di importazione
+        try
+        {
+            ProcessStartInfo psi = new ProcessStartInfo() { FileName = iQuoteConsole, Arguments = "Remote -j ImportData Hendi", Verb = "runas", UseShellExecute = false };
+            Process p = new Process() { StartInfo = psi };
+            p.Start();
+            p.WaitForExit();
+        }
+        catch (Exception)
+        {
+            
+        }
+    }
+
+    internal static void DownloadAsync(string url, string localPath, List<string> downloadedFile, Guid? threadId)
+    {
+        using (var client = new WebClient())
+        {
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                client.DownloadFile(url, localPath);
+                downloadedFile.Add(localPath);
+            }
+            catch (Exception)
+            {
+                //if (threadId != null)
+                //    new DataExchange.DataExchangeWriteLog(threadId) { ProcessName = "Downloading File: " + Path.GetFileName(localPath), ErrorID = 5495, ErrorMessage = ex.Message }.WriteToLog();
+            }
+
+        }
     }
 }
